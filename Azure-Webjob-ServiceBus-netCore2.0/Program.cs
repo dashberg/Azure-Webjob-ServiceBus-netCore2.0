2d000b7fbc74cd13cd10f980fd8d417512e5ffc8
Daniel Ashberger <23121803+dashberg@users.noreply.github.com>
2018-04-10 16:14:50 +0200

    Update Program.cs

--- Azure-Webjob-ServiceBus-netCore2.0/Program.cs
+++ Azure-Webjob-ServiceBus-netCore2.0/Program.cs
@@ -20,9 +20,10 @@
       {        
         // SQLAZURECONNSTR_DefaultConnection - is a Azure notification
         var defaultConnection = Environment.GetEnvironmentVariable("SQLAZURECONNSTR_DefaultConnection", EnvironmentVariableTarget.Process);
-        serviceCollection.AddDbContext<FDSContext>(sharedOptions => sharedOptions.UseSqlServer(defaultConnection));
+        // Serve up the dbcontext
+        serviceCollection.AddDbContext<XXXContext>(sharedOptions => sharedOptions.UseSqlServer(defaultConnection));
         
-        serviceCollection.AddScoped<IVistaOrderService, VistaOrderSerivce>();       
+        serviceCollection.AddScoped<IXXOrderService, XXXOrderSerivce>();       
         serviceCollection.AddScoped<WebJobsMethods, WebJobsMethods>();
 
         // Set from configfile or read from Azure Application Settings
